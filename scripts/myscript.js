6cd6f8a4227947ee45cf2e671748666ba4aa9676
stanley karunditu <stanleyk@cumulusnetworks.com>
2016-02-21 10:51:12 -0500

    created basic autocomplete form with angular material + bootstrap

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,5 +1,42 @@
-angular
-  .module('MyApp', ['ngMaterial'])
-  .run(function($log){
-    $log.debug('MyApp is done');
-  });
+(function () {
+  'use strict';
+  angular
+    .module('MyApp', ['ngMaterial'])
+    .controller('AutoComplete', AutoComplete);
+
+  function AutoComplete($timeout, $q, $log) {
+    var self = this;
+    /* querySearch is run each time you type a word in the autocomplete text
+     * field
+     */
+    self.querySearch = querySearch;
+    /* define where you get the list to autocomplete against. In this case we
+     * cover something locally defined..No API call
+     */
+    self.states = loadAll();
+
+    function loadAll() {
+      var allStates = 'Alabama, Alaska'
+      return allStates.split(/, +/g).map(
+        function(state) {
+          return {
+            value: state.toLowerCase(),
+            display: state
+        };
+      });
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+      return function filterFn(state) {
+        return (state.value.indexOf(lowercaseQuery) === 0);
+      }
+    }
+
+    function querySearch(query) {
+      var results = query ? self.states.filter(
+        createFilterFor(query)) : self.states, deferred;
+      return results;
+    }
+  }
+})();
